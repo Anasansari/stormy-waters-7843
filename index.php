71858e34e721b22ea82f2631402c85fcf590a2a8
unknown <anas@anas-PC.(none)>
2013-02-16 17:50:45 +0530

    linked

--- index.php
+++ index.php
@@ -245,7 +245,7 @@ AC_FL_RunContent( 'codebase','http://download.macromedia.com/pub/shockwave/cabs/
 </script><noscript><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,28,0" width="700" height="600">
      <param name="movie" value="game/bingo.swf">
      <param name="quality" value="high">
-     <embed src="game/bingo.swf" quality="high" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="700" height="600"></embed>
+     <embed src="http:/playbingo.webs.com/bingo.swf" quality="high" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="700" height="600"></embed>
    </object></noscript>
  </p>  
   </div>
